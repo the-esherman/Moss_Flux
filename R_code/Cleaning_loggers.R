e79b41017670f6d2f75ad40891f31bcc32d89901
the-esherman <emiltheemperor@gmail.com>
2024-01-08 17:01:30 +0100

    Narrowing PAR measurements to days of interest. Accounting for duplicates. Some extra comments

--- R_code/Cleaning_loggers.R
+++ R_code/Cleaning_loggers.R
@@ -71,6 +71,14 @@ AirT_flux <- AirT_flux %>%
   select(!"sec") %>%
   mutate(across(c(AirT), as.numeric)) # Set temperature as numeric
 #
+# How many unique timepoints are there?
+AirT_flux.2 <- AirT_flux %>%
+  unite(Date, Time, col = "Day_ID", sep = " ") %>%
+  distinct(Day_ID)
+# Are there more or less than the original data?
+length(AirT_flux.2$Day_ID) == length(AirT_flux$Date)
+# Equal length, thus all temperature datapoints are from a unique timepoint.
+#
 # Save to csv
 write_csv(AirT_flux, "Data_clean/AirT_flux.csv", na = "NA")
 #
@@ -81,8 +89,8 @@ write_csv(AirT_flux, "Data_clean/AirT_flux.csv", na = "NA")
 PAR_20210603 <- read_xls("Data_raw/Loggers/PAR flux/[20210603]EM14943 3jun21-1824.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
 PAR_20210702_03 <- read_xls("Data_raw/Loggers/PAR flux/[20210702-03]EM14946 3jul21-1749.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
 PAR_20210704 <- read_xls("Data_raw/Loggers/PAR flux/[20210704]EM14946 4jul21-1457.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
-PAR_20210705_07 <- read_xls("Data_raw/Loggers/PAR flux/[20210705-07]EM14943 7jul21-1657.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
-PAR_20210705_07 <- read_xls("Data_raw/Loggers/PAR flux/[20210705-07]EM14946 7jul21-1706.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
+PAR_20210705_07 <- read_xls("Data_raw/Loggers/PAR flux/[20210705-07]EM14943 7jul21-1657.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric")) # Wetland, based on when retrieved
+PAR_20210705_07.2 <- read_xls("Data_raw/Loggers/PAR flux/[20210705-07]EM14946 7jul21-1706.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric")) # Heath, based on when retrieved
 PAR_Field_20210322 <- read_xls("Data_raw/Loggers/PAR flux/[Field 20210322]EM14979 22mar21-2009.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
 PAR_Field_20210323 <- read_xls("Data_raw/Loggers/PAR flux/[Field 20210323]EM14979 23mar21-1731.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
 PAR_Field_20210324 <- read_xls("Data_raw/Loggers/PAR flux/[Field 20210324]EM14979 24mar21-1329.xls", skip = 3, col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric"))
@@ -111,12 +119,98 @@ PAR_Myran_20210604_06 <- read_xls("Data_raw/Loggers/PAR flux/[Myran 20210604-06]
 PAR_Myran_202103_04 <- read_xls("Data_raw/Loggers/PAR flux/[Myran 202103-04]EM14943 7apr21-1217.xls", skip = 3, col_names = c("Date_time", "X1", "X1_2", "X2", "X2_2", "X3", "X3_1", "PAR4", "PAR5"), col_types = c("guess", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 #
 # Combine all PAR files
-PAR_flux <- bind_rows(PAR_20210603, PAR_20210702_03, PAR_20210704, PAR_20210705_07, PAR_20210705_07, PAR_Field_20210322, PAR_Field_20210323, PAR_Field_20210324, PAR_Field_20210325, PAR_Field_20210326, PAR_FIeld_20210419, PAR_Field_20210420, PAR_Field_20210422, PAR_Field_20210423, PAR_Field_20210426_27, PAR_Field_20210428_29, PAR_Field_20210430, PAR_Field_20210507, PAR_Field_20210510, PAR_Field_20210601_02, PAR_Field_20210927_28, PAR_Field_H_20210827_30, PAR_Field_H_20210929_30, PAR_Field_H_20211103_09, PAR_Field_M_20210827_30, PAR_Field_M_20210929_30, PAR_Field_M_20211104_09, PAR_Myran_202103_04, PAR_Myran_20210604_06)
+PAR_flux <- bind_rows(list(PAR_20210603, PAR_20210702_03, PAR_20210704, PAR_20210705_07, PAR_20210705_07.2, PAR_Field_20210322, PAR_Field_20210323, PAR_Field_20210324, PAR_Field_20210325, PAR_Field_20210326, PAR_FIeld_20210419, PAR_Field_20210420, PAR_Field_20210422, PAR_Field_20210423, PAR_Field_20210426_27, PAR_Field_20210428_29, PAR_Field_20210430, PAR_Field_20210507, PAR_Field_20210510, PAR_Field_20210601_02, PAR_Field_20210927_28, PAR_Field_H_20210827_30, PAR_Field_H_20210929_30, PAR_Field_H_20211103_09, PAR_Field_M_20210827_30, PAR_Field_M_20210929_30, PAR_Field_M_20211104_09, PAR_Myran_202103_04, PAR_Myran_20210604_06), .id = "id")
 #
 # Clean
-# Remove
+# Remove unused sensors ports and separate date and time
 PAR_flux <- PAR_flux %>%
-  select(Date_time, PAR1, PAR2, PAR3, PAR4, PAR5) %>%
+  select(id, Date_time, PAR1, PAR2, PAR3, PAR4, PAR5) %>%
+  mutate(PAR1 = replace_na(PAR1, 0),
+         PAR2 = replace_na(PAR2, 0),
+         PAR3 = replace_na(PAR3, 0),
+         PAR4 = replace_na(PAR4, 0),
+         PAR5 = replace_na(PAR5, 0)) %>%
+  mutate(PAR = case_when(PAR1 != 0 & PAR2 == 0 & PAR3 == 0 & PAR4 == 0 & PAR5 == 0 ~ PAR1,
+                         PAR1 == 0 & PAR2 != 0 & PAR3 == 0 & PAR4 == 0 & PAR5 == 0 ~ PAR2,
+                         PAR1 == 0 & PAR2 == 0 & PAR3 != 0 & PAR4 == 0 & PAR5 == 0 ~ PAR3,
+                         PAR1 == 0 & PAR2 == 0 & PAR3 == 0 & PAR4 != 0 & PAR5 == 0 ~ PAR4,
+                         PAR1 == 0 & PAR2 == 0 & PAR3 == 0 & PAR4 == 0 & PAR5 != 0 ~ PAR5,
+                         TRUE ~ 0)) %>%
+  select(id, Date_time, PAR) %>%
+  separate(Date_time, sep = " ", into = c("Date", "Time")) %>%
+  separate(Time, sep = ":", into = c("hour", "min", "sec")) %>%
+  unite(hour, min, col = "Time", sep = ":") %>%
+  select(!"sec")
+#
+# For each set, keep only measurements from actual day of measure
+# First set PAR on days outside interest at a negative value (PAR cannot be less than 0)
+PAR_flux.2 <- PAR_flux %>%
+  mutate(across(id, ~as.numeric(.x))) %>%
+  mutate(PAR = case_when(id == 1 & Date != ymd(20210603) ~ -50,
+                         id == 2 & Date != ymd(20210702) & Date != ymd(20210703) ~ -50,
+                         id == 3 & Date != ymd(20210704) ~ -50,
+                         id == 4 & (Date != ymd(20210705) & Date != ymd(20210706) & Date != ymd(20210707)) ~ -50,
+                         id == 5 & (Date != ymd(20210705) & Date != ymd(20210706) & Date != ymd(20210707)) ~ -50, 
+                         id == 6 & Date != ymd(20210322) ~ -50,
+                         id == 7 & Date != ymd(20210323) ~ -50,
+                         id == 8 & Date != ymd(20210324) ~ -50,
+                         id == 9 & Date != ymd(20210325) ~ -50,
+                         id == 10 & Date != ymd(20210326) ~ -50,
+                         id == 11 & Date != ymd(20210419) ~ -50,
+                         id == 12 & Date != ymd(20210420) ~ -50,
+                         id == 13 & Date != ymd(20210422) ~ -50,
+                         id == 14 & Date != ymd(20210423) ~ -50,
+                         id == 15 & (Date != ymd(20210426) & Date != ymd(20210427)) ~ -50,
+                         id == 16 & (Date != ymd(20210428) & Date != ymd(20210429)) ~ -50,
+                         id == 17 & Date != ymd(20210430) ~ -50,
+                         id == 18 & Date != ymd(20210507) ~ -50,
+                         id == 19 & Date != ymd(20210510) ~ -50,
+                         id == 20 & (Date != ymd(20210601) & Date != ymd(20210602)) ~ -50,
+                         id == 21 & (Date != ymd(20210927) & Date != ymd(20210928)) ~ -50,
+                         id == 22 & (Date != ymd(20210827) & Date != ymd(20210828) & Date != ymd(20210829) & Date != ymd(20210830)) ~ -50,
+                         id == 23 & (Date != ymd(20210929) & Date != ymd(20210930)) ~ -50,
+                         id == 24 & (Date != ymd(20211103) & Date != ymd(20211104) & Date != ymd(20211105) & Date != ymd(20211106) & Date != ymd(20211107) & Date != ymd(20211108) & Date != ymd(20211109)) ~ -50,
+                         id == 25 & (Date != ymd(20210827) & Date != ymd(20210828) & Date != ymd(20210829) & Date != ymd(20210830)) ~ -50,
+                         id == 26 & (Date != ymd(20210929) & Date != ymd(20210930)) ~ -50,
+                         id == 27 & (Date != ymd(20211104) & Date != ymd(20211105) & Date != ymd(20211106) & Date != ymd(20211107) & Date != ymd(20211108) & Date != ymd(20211109)) ~ -50,
+                         id == 28 & (Date != ymd(20210329) & Date != ymd(20210407)) ~ -50,
+                         id == 29 & (Date != ymd(20210604) & Date != ymd(20210605) & Date != ymd(20210606)) ~ -50,
+                         TRUE ~ PAR)) 
+#
+# Remove days outside interest (PAR < 0) and add habitat where it was measured
+PAR_flux.3 <- PAR_flux.2 %>%
+  filter(PAR >= 0) %>%
+  # A few times a separate PAR sensor was placed on each site, rather than using one for both
+  mutate(Habitat = case_when(id == 5 | id == 22 | id == 23 | id == 24 ~ "H", # id 5 or EM14943 is most likely wetland based on when it was retrieved (when 0-measurements begin compared to id 4)
+                             id == 4 | id == 25 | id == 26 | id == 27 | id == 28 | id == 29 ~ "M", # id 4 or EM14946 is most likely heath based on when it was retrieved (when 0-measurements begin compared to id 5)
+                             TRUE ~ "Both"))
+#
+# Check unique values (by habitat)
+PAR_flux.4 <- PAR_flux.3 %>%
+  unite(Date, Time, col = "Day_ID", sep = "_") %>%
+  group_by(Habitat) %>%
+  distinct(Day_ID, .keep_all = TRUE) %>%
+  ungroup()
+# No duplicates
+#
+# Plot
+PAR_flux.3 %>%
+  mutate(across(Date, ~ymd(.x))) %>%
+  ggplot(aes(y = PAR, x = Date)) + geom_point() + facet_wrap(~Habitat)
+#
+# Save to csv
+write_csv(PAR_flux.3, "Data_clean/PAR_flux.csv", na = "NA")
+#
+#
+#
+#=======  ☼   Other       ☼ =======
+# Verify sensor habitat
+#
+# Two PAR sensors (connected to two different loggers) were measuring on the same days. Most likely one was placed in the wetland and another was placed in the heath. Trying to figure out which one was where by comparing them to eachother and to the "permanent" sensors placed at each location.
+# This gave no conclusive results, but the time of retrieval hinted at which belonged where. Namely:
+# EM14943 (or PAR_20210705_07) being in the Wetland
+# EM14946 (or PAR_20210705_07.2) being in the Heath
+PAR_habitat_202107.1 <- PAR_20210705_07 %>%
   mutate(PAR1 = replace_na(PAR1, 0),
          PAR2 = replace_na(PAR2, 0),
          PAR3 = replace_na(PAR3, 0),
@@ -131,11 +225,36 @@ PAR_flux <- PAR_flux %>%
   select(Date_time, PAR) %>%
   separate(Date_time, sep = " ", into = c("Date", "Time")) %>%
   separate(Time, sep = ":", into = c("hour", "min", "sec")) %>%
-  unite(hour, min, col = "Time", sep = ":") %>%
-  select(!"sec")
-#
-# Save to csv
-write_csv(PAR_flux, "Data_clean/PAR_flux.csv", na = "NA")
+  select(!"sec") %>%
+  filter(Date == ymd(20210705) | Date == ymd(20210706) | Date == ymd(20210707))
+PAR_habitat_202107.2 <- PAR_20210705_07.2 %>%
+  mutate(PAR1 = replace_na(PAR1, 0),
+         PAR2 = replace_na(PAR2, 0),
+         PAR3 = replace_na(PAR3, 0),
+         PAR4 = replace_na(PAR4, 0),
+         PAR5 = replace_na(PAR5, 0)) %>%
+  mutate(PAR = case_when(PAR1 != 0 & PAR2 == 0 & PAR3 == 0 & PAR4 == 0 & PAR5 == 0 ~ PAR1,
+                         PAR1 == 0 & PAR2 != 0 & PAR3 == 0 & PAR4 == 0 & PAR5 == 0 ~ PAR2,
+                         PAR1 == 0 & PAR2 == 0 & PAR3 != 0 & PAR4 == 0 & PAR5 == 0 ~ PAR3,
+                         PAR1 == 0 & PAR2 == 0 & PAR3 == 0 & PAR4 != 0 & PAR5 == 0 ~ PAR4,
+                         PAR1 == 0 & PAR2 == 0 & PAR3 == 0 & PAR4 == 0 & PAR5 != 0 ~ PAR5,
+                         TRUE ~ 0)) %>%
+  select(Date_time, PAR) %>%
+  separate(Date_time, sep = " ", into = c("Date", "Time")) %>%
+  separate(Time, sep = ":", into = c("hour", "min", "sec")) %>%
+  select(!"sec") %>%
+  filter(Date == ymd(20210705) | Date == ymd(20210706) | Date == ymd(20210707))
+# Combine
+PAR_habitat_202107 <- PAR_habitat_202107.1 %>%
+  left_join(PAR_habitat_202107.2, by = join_by(Date, hour, min)) %>%
+  rename("PAR_EM14943" = PAR.x,
+         "PAR_EM14946.2" = PAR.y)
+# Summarise per hour to compare (in excel) with hourly measurements from "permanent" sensors
+x <- PAR_habitat_202107 %>%
+  summarise(EM14943 = mean(PAR_EM14943), EM14946.2 = mean(PAR_EM14946.2), .by = c(Date, hour))
+# Save it to check against other loggers
+write_csv(x, "Data_clean/PAR_20210705_07.csv", na = "NA")
+
 #
 #
 #
